f46d56c2ca9c13ab6af6fef61f73ab047fbf7aa4
cheny43 <1508638510@qq.com>
2022-01-29 00:44:01 +0800

    static_pages

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
-Route::get('/',[App\Http\Controllers\StaticPagesController::class,'home']);
-
-// Route::get('/', 'StaticPagesController@home');
+Route::get('/', 'StaticPagesController@home');
 Route::get('/help', 'StaticPagesController@help');
 Route::get('/about', 'StaticPagesController@about');
 
