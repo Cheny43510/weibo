1a41854f25353cfd68568841b3223e81c5d7fc72
cheny43 <1508638510@qq.com>
2022-01-30 21:20:17 +0800

    切割头部和尾部子视图

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,23 +1,19 @@
 <!DOCTYPE html>
-<html>
-  <head>
-    <title>@yield('title', 'Weibo App') - Laravel 入门教程</title>
-    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-  </head>
-  <body>
-
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div class="container">
-        <a class="navbar-brand" href="/">Weibo App</a>
-        <ul class="navbar-nav justify-content-end">
-          <li class="nav-item"><a class="nav-link" href="/help">帮助</a></li>
-          <li class="nav-item" ><a class="nav-link" href="#">登录</a></li>
-        </ul>
-      </div>
-    </nav>
-
-    <div class="container">
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>@yield('title','Weibo App') - Laravel 入门教程</title>
+  <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+</head>
+<body>
+  @include('layouts._header')
+  <div class="container">
+    <div class="offset-md-1 col-md-10">
       @yield('content')
+      @include('layouts._footer')
     </div>
-  </body>
+  </div>
+</body>
 </html>
