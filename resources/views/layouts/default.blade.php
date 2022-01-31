ea3c21d86f14380d65aba85e486beb90363afe3d
cheny43 <1508638510@qq.com>
2022-01-31 16:30:51 +0800

    完成用户注册功能

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,19 +1,19 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>@yield('title','Weibo App') - Laravel 入门教程</title>
-  <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-</head>
-<body>
-  @include('layouts._header')
-  <div class="container">
-    <div class="offset-md-1 col-md-10">
-      @yield('content')
-      @include('layouts._footer')
+<html>
+  <head>
+    <title>@yield('title', 'Weibo App') - Laravel 入门教程</title>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+  </head>
+
+  <body>
+    @include('layouts._header')
+
+    <div class="container">
+      <div class="offset-md-1 col-md-10">
+        @include('shared._messages')
+        @yield('content')
+        @include('layouts._footer')
+      </div>
     </div>
-  </div>
-</body>
+  </body>
 </html>
