e832d06a57e3ae11a8b90d73f8bc160890450e3d
cheny43 <1508638510@qq.com>
2022-02-02 12:36:32 +0800

    登录时记住我

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -22,21 +22,26 @@ class SessionsController extends Controller
     * @param Request $request
     * @return void
     */
-    public function store(Request $request){
-        $credentials = $this->validate($request,[
-            'email' => 'required|email|max:255',
-            'password' => 'required'
-        ]);
+    public function store(Request $request)
+    {
+       $credentials = $this->validate($request, [
+           'email' => 'required|email|max:255',
+           'password' => 'required'
+       ]);
 
-        if (Auth::attempt($credentials)) {
-            session()->flash('success', '欢迎回来！');
-            return redirect()->route('users.show', [Auth::user()]);
-        } else {
-            session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
-            return redirect()->back()->withInput();
-        }
+       if (Auth::attempt($credentials, $request->has('remember'))) {
+           session()->flash('success', '欢迎回来！');
+           return redirect()->route('users.show', [Auth::user()]);
+       } else {
+           session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
+           return redirect()->back()->withInput();
+       }
     }
-
+    /**
+     * 退出(销毁会话)
+     *
+     * @return void
+     */
     public function destroy(){
         Auth::logout();
         session()->flash('success', '您已成功退出！');
