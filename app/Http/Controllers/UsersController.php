176f206ebf65827124d114bdc456f84eb9406271
cheny43 <1508638510@qq.com>
2022-02-02 13:06:45 +0800

    更改用户资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -39,9 +39,46 @@ class UsersController extends Controller
             'password' => bcrypt($request->password),
         ]);
 
-        
+
         Auth::login($user);
         session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
         return redirect()->route('users.show', [$user]);
+    }
+
+    /**
+     * 显示用户信息编辑页面
+     *
+     * @param User $user
+     * @return void
+     */
+    public function edit(User $user)
+    {
+        return view('users.edit', compact('user'));
+    }
+
+    /**
+     * 执行用户信息逻辑
+     *
+     * @param User $user
+     * @param Request $request
+     * @return void
+     */
+    public function update(User $user, Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'password' => 'nullable|confirmed|min:6'
+        ]);
+
+        $data = [];
+        $data['name'] = $request->name;
+        if ($request->password) {
+            $data['password'] = bcrypt($request->password);
+        }
+        $user->update($data);
+
+        session()->flash('success', '个人资料更新成功！');
+
+        return redirect()->route('users.show', $user);
     }
 }
