082806dc3b1b9cbdbfc4c6d5b85218bf8b17f3c3
cheny43 <1508638510@qq.com>
2022-02-02 13:47:13 +0800

    用户列表

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -15,7 +15,7 @@ class UsersController extends Controller
          * 用户认证,必须先登录
          */
         $this->middleware('auth', [
-            'except' => ['show', 'create', 'store']
+            'except' => ['show', 'create', 'store','index']
         ]);
     }
     /**
@@ -92,5 +92,15 @@ class UsersController extends Controller
         session()->flash('success', '个人资料更新成功！');
 
         return redirect()->route('users.show', $user);
+    }
+    /**
+     * Undocumented function
+     *
+     * @return void
+     */
+    public function index()
+    {
+        $users = User::paginate(6);
+        return view('users.index', compact('users'));
     }
 }
